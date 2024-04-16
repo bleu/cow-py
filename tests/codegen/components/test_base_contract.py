ff68306ccf00418ce6443843502ef297a93474ae
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-04-16 19:00:41 -0300

    fix: base contract tests

--- tests/codegen/components/test_base_contract.py
+++ tests/codegen/components/test_base_contract.py
@@ -3,13 +3,10 @@ from unittest.mock import patch, Mock
 from cow_py.codegen.components.base_contract import (
     BaseContract,
     Chain,
-)  # Adjust the import statement according to your actual module structure
+)
 
 
-# Test for Singleton Pattern
-@patch(
-    "cow_py.codegen.components.base_contract.ContractLoader"
-)  # Mocking ContractLoader to avoid actual web3 calls
+@patch("cow_py.codegen.components.base_contract.ContractLoader")
 def test_base_contract_singleton(mock_loader):
     address = "0x123"
     chain = Chain.MAINNET
@@ -20,6 +17,15 @@ def test_base_contract_singleton(mock_loader):
     ), "BaseContract should return the same instance for the same address and chain"
 
 
+class MockWithoutAttributes(Mock):
+    # By default Mock objects allow access to any attribute, even if it doesn't exist.
+    # on the Base Contract class, we want to raise an AttributeError if the attribute doesn't exist.
+    def __getattr__(self, name: str):
+        if name == "balanceOf" or name == "nonExistentMethod":
+            raise AttributeError()
+        return super().__getattr__(name)
+
+
 @pytest.fixture
 def contract_with_abi():
     abi = [
@@ -27,10 +33,14 @@ def contract_with_abi():
         {"type": "event", "name": "Transfer"},
     ]
     with patch("cow_py.codegen.components.base_contract.ContractLoader") as mock_loader:
-        mock_contract = Mock()
+        mock_contract = MockWithoutAttributes()
         mock_contract.abi = abi
+        mock_contract.functions = Mock(
+            balanceOf=Mock(return_value=Mock(call=Mock(return_value="1000"))),
+        )
+
         mock_loader.return_value.get_web3_contract.return_value = mock_contract
-        contract = BaseContract("0x123", Chain.MAINNET, abi)
+        contract = BaseContract("0x456", Chain.MAINNET, abi)
     return contract
 
 
@@ -39,30 +49,12 @@ def test_base_contract_function_exists_in_abi(contract_with_abi):
     assert not contract_with_abi._function_exists_in_abi("transfer")
 
 
-# def test_base_contract_event_exists_in_abi(contract_with_abi):
-#     assert contract_with_abi._event_exists_in_abi("Transfer")
-#     assert not contract_with_abi._event_exists_in_abi("Approval")
+def test_base_contract_event_exists_in_abi(contract_with_abi):
+    assert contract_with_abi._event_exists_in_abi("Transfer")
+    assert not contract_with_abi._event_exists_in_abi("Approval")
 
 
-# @patch("cow_py.codegen.components.base_contract.ContractLoader")
-# def test_base_contract_getattr(mock_loader):
-#     mock_contract = Mock()
-#     mock_contract.address = "0x123"
-#     mock_contract.abi = [
-#         {"type": "function", "name": "balanceOf"},
-#         {"type": "event", "name": "Transfer"},
-#     ]
-#     mock_contract.functions.balanceOf = Mock(
-#         return_value=Mock(call=Mock(return_value="1000"))
-#     )
-#     mock_contract.events.Transfer = Mock(filter=Mock(return_value=[]))
-
-#     mock_loader.return_value.get_web3_contract.return_value = mock_contract
-
-#     contract = BaseContract("0x123", Chain.MAINNET)
-#     assert (
-#         contract.balanceOf() == "1000"
-#     ), "Failed to mock balanceOf function call properly"
-#     assert hasattr(contract, "Transfer"), "Event 'Transfer' should be accessible"
-#     with pytest.raises(AttributeError):
-#         _ = contract.nonExistentMethod
+def test_base_contract_getattr(contract_with_abi):
+    assert contract_with_abi.balanceOf() == "1000"
+    with pytest.raises(AttributeError):
+        _ = contract_with_abi.nonExistentMethod
